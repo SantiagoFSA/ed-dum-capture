2aed3b4eb04d25ccf7be7c9dc8a3b85ea14a03d4
Santiago F Santos <santiagofs@avalith.net>
2021-01-22 11:38:35 -0300

    5. styled components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+//Global Style
+import GlobalStyle from "./components/GlobalStyle";
+
 //import pages
 import AboutSection from "./components/AboutSection";
 import AboutUs from "./pages/AboutUs";
@@ -5,6 +8,7 @@ import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
