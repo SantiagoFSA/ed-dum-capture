7adbb08f9de49b713065623b26ae2f5d10d853ab
Santiago F Santos <santiagofs@avalith.net>
2021-01-26 12:30:18 -0300

    ? - movie details / framer motion

--- src/App.js
+++ src/App.js
@@ -4,14 +4,32 @@ import GlobalStyle from "./components/GlobalStyle";
 //import pages
 import AboutSection from "./components/AboutSection";
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
+//Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
